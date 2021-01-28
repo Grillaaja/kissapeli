3086c9504b3e91d3735e0fa7e00d296021ceeeba
Grillaaja <santeri.jarvenpaa22@gmail.com>
2021-01-28 13:51:59 +0200

    miau

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector3(hor * speed, ver * speed, 0);
+        
+    }
+}
