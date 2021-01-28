bac6281d3adc7efc6ca3a6507643b64ec444625e
Grillaaja <santeri.jarvenpaa22@gmail.com>
2021-01-28 15:09:40 +0200

    juttui

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
     Rigidbody2D rigidbody;
-
+    public Text collectedText;
+    public static int collectedAmount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,6 @@ public class PlayerMovement : MonoBehaviour
         float ver = Input.GetAxis("Vertical");
 
         rigidbody.velocity = new Vector3(hor * speed, ver * speed, 0);
-        
+        collectedText.text = "Items collected: " + collectedAmount;
     }
 }
