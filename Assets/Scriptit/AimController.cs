322dc3cb4a2c1ec4b30e9a25c87ed7591cd5a885
Grillaaja <santeri.jarvenpaa22@gmail.com>
2021-02-04 14:21:59 +0200

    mmm

---
+++ Assets/Scriptit/AimController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+
+    private Camera cam;
+
+    public Transform firePoint;
+    public GameObject projectile;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 screenPoint = cam.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Input.GetMouseButtonDown(0)){
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
+        }
+    }
+}
---
+++ Assets/Scriptit/PlayerProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            //vihun hp pois
+            Destroy(gameObject);
+        }else if (collision.tag == "Player")
+        {
+            //eimitää
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
